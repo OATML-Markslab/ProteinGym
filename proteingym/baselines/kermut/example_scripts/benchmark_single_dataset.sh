f33c87a2af052ae6012b268d806b221689c85650
Peter Mørch Groth <petermoerchgroth@gmail.com>
2024-09-22 14:48:26 -0400

    Remove unnecessary files

--- proteingym/baselines/kermut/example_scripts/benchmark_single_dataset.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python src/experiments/proteingym_benchmark.py --multirun \
-    dataset=TCRG1_MOUSE_Tsuboyama_2023_1E0L \
-    split_method=fold_random_5,fold_modulo_5,fold_contiguous_5 \
-    gp=kermut
--- proteingym/baselines/kermut/example_scripts/zero_shot_scores.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python src/data/extract_esm2_zero_shots.py --dataset all
