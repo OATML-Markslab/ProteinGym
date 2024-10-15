4d3d3914ae03517338694c0c74b4cd15d84645cb
pascalnotin <pascal.notin@gmail.com>
2024-10-15 17:34:51 -0400

    PG_v1.0_init

--- proteingym/baselines/PoET/poet/models/modules/__init__.py
+++
@@ -1,6 +0,0 @@
-from .activation import *
-from .attention import *
-from .attention_flash import *
-from .embedding import *
-from .transformer import *
-from .transformer_rotary import *
--- proteingym/baselines/protssn/src/module/egnn/__init__.py
+++
@@ -1,2 +0,0 @@
-from .egnn_pytorch import EGNN, EGNN_Network
-from .egnn_pytorch_geometric import EGNN_Sparse, EGNN_Sparse_Network
