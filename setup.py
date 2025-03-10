b683966e22ad9c096e3d662e5d8323e6c240791a
pascalnotin <pascal.notin@gmail.com>
2025-03-10 14:33:14 -0400

    ProteinGym v1.2 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     author="OATML-Markslab",
-    version="1.1",
+    version="1.2",
     license="MIT",
     url="https://github.com/OATML-Markslab/ProteinGym",    
     packages=["proteingym"]
