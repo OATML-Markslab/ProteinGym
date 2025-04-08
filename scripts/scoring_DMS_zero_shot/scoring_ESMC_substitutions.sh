4fa772b33df1b0d9faae3f074a795d7199bb91e4
Pascal Notin <pascal.notin@stx.ox.ac.uk>
2025-04-08 21:33:42 +0100

    Added ESMC scoring script

--- scripts/scoring_DMS_zero_shot/scoring_ESMC_substitutions.sh
+++ scripts/scoring_DMS_zero_shot/scoring_ESMC_substitutions.sh
@@ -3,12 +3,10 @@
 source ../zero_shot_config.sh
 source activate proteingym_env
 
-export dms_output_folder=${DMS_output_score_folder_subs}/ESM3/ 
+export dms_output_folder=${DMS_output_score_folder_subs}/ESM_C/600M
 
 python ../../proteingym/baselines/evoscale/compute_fitness.py \
-    --model_type "esm3_open" \
+    --model_type "esmc_600M" \
     --reference_csv ${DMS_reference_file_path_subs} \
     --dms_dir ${DMS_data_folder_subs} \
-    --pdb_dir ${DMS_structure_folder} \
-    --output_dir ${dms_output_folder} \
-    --use_structure
+    --output_dir ${dms_output_folder}
