98dc8d2aebb3f1ab1d47d650ed543ecd5898d9dc
Yilan Wang <yilanwang@Yilans-MBP.home.kola.sh>
2026-02-09 19:32:11 -0500

    refactor the code to take in input as other models in PG

--- proteingym/baselines/AIDO/compute_fitness.py
+++ proteingym/baselines/AIDO/compute_fitness.py
@@ -24,6 +24,7 @@ from scipy.special import softmax
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+sys.path.append("/n/groups/marks/users/elain/PG2/ProteinGym/AIDO/")
 from utils import misc
 from utils import protein
 
@@ -67,54 +68,108 @@ def main(args):
     #############################
 
     os.makedirs(args.output_path, exist_ok=True)
-    for dms_id in args.dms_ids:
-        
-        ## Read Sequence
-        dms2seq, dms2annot = misc.load_fasta(f"{args.input_data_path}/query.fasta", load_annotation=True)
-        q_seq = dms2seq[dms_id]
-        start, end = [int(x) for x in dms2annot[dms_id].split('-')]
 
-        ## Read MSA
-        msa = misc.load_msa_txt(f"{args.input_data_path}/msa_data/{dms_id}.txt.gz")
-        assert q_seq == msa[0]
-
-        ## Read PDB
-        with open(f"{args.input_data_path}/struc_data/{dms_id}.pdb") as IN:
-            text = IN.read()
-
-        prot = protein.from_pdb_string(text, molecular_type='protein')
-        assert prot.seq(True) == q_seq
-        
-        ## Read DMS Table
-        dms_df = pd.read_csv(f'{args.input_data_path}/dms_data/{dms_id}.csv')
+    if args.dms_ids != None:
+        for dms_id in args.dms_ids:
+            
+            ## Read Sequence
+            dms2seq, dms2annot = misc.load_fasta(f"{args.fasta_dir}/{dms_id}.fasta", load_annotation=True)
+            q_seq = dms2seq[dms_id]
+            start, end = [int(x) for x in dms2annot[dms_id].split('-')]
     
-        ## Inference
-        all_poses, logits_table = misc.get_logits_table_sliding(q_seq, prot, msa, dms_df, model, tokenizer, str_tokenizer, start, mask_str=args.mask_str, disable_tqdm=False)
-        result_df = misc.get_scores_from_table(q_seq, logits_table, all_poses, dms_df, tokenizer, start, temp_mt=1.0, temp_wt=1.5)
+            ## Read MSA
+            msa = misc.load_msa_txt(f"{args.msa_dir}/{dms_id}.a2m") # msa filename
+            assert q_seq == msa[0]
+    
+            ## Read PDB
+            with open(f"{args.pdb_dir}/{dms_id}.pdb") as IN: # pdb filename
+                text = IN.read()
+    
+            prot = protein.from_pdb_string(text, molecular_type='protein')
+            assert prot.seq(True) == q_seq
+            
+            ## Read DMS Table
+            dms_df = pd.read_csv(f'{args.dms_dir}/{dms_id}.csv') # dms filename
         
-        assert np.all(dms_df['mutant'] == result_df['Mutation'])
-        assert np.allclose(dms_df['DMS_score'] , result_df['GT_Score'], 1e-05, 1e-05)
-        dms_df['AIDO.Protein-RAG-16B-zeroshot'] = result_df['Pred_Score']
-        result_df = dms_df
+            ## Inference
+            all_poses, logits_table = misc.get_logits_table_sliding(q_seq, prot, msa, dms_df, model, tokenizer, str_tokenizer, start, mask_str=args.mask_str, disable_tqdm=False)
+            result_df = misc.get_scores_from_table(q_seq, logits_table, all_poses, dms_df, tokenizer, start, temp_mt=1.0, temp_wt=1.5)
+            
+            assert np.all(dms_df['mutant'] == result_df['Mutation'])
+            assert np.allclose(dms_df['DMS_score'] , result_df['GT_Score'], 1e-05, 1e-05)
+            dms_df['AIDO.Protein-RAG-16B-zeroshot'] = result_df['Pred_Score']
+            result_df = dms_df
+            
+            result_df.to_csv(join(args.output_path, f"{dms_id}.csv"), index=False)
+            r = round(spearmanr(result_df['AIDO.Protein-RAG-16B-zeroshot'], result_df['DMS_score'])[0], 4)
+            print(f"{dms_id}: R={r}")
+    elif args.reference_file != None:
+        reference_file_df = pd.read_csv(args.reference_file)
+        for index, row in reference_file_df.iterrows():
+            dms_id = row['DMS_id']
+
+            ## Read Sequence
+            dms2seq, dms2annot = misc.load_fasta(f"{args.fasta_dir}/{dms_id}.fasta", load_annotation=True)
+            q_seq = dms2seq[dms_id]
+            start, end = [int(x) for x in dms2annot[dms_id].split('-')]
+    
+            ## Read MSA
+            msa = misc.load_msa_txt(f"{args.msa_dir}/{row['MSA_filename']}")
+            assert q_seq == msa[0]
+    
+            ## Read PDB
+            with open(f"{args.pdb_dir}/{row['pdb_file']}") as IN: # pdb filename
+                text = IN.read()
+    
+            prot = protein.from_pdb_string(text, molecular_type='protein')
+            assert prot.seq(True) == q_seq
+            
+            ## Read DMS Table
+            dms_df = pd.read_csv(f'{args.dms_dir}/{dms_id}.csv') # dms filename
         
-        result_df.to_csv(join(args.output_path, f"{dms_id}.csv"), index=False)
-        r = round(spearmanr(result_df['AIDO.Protein-RAG-16B-zeroshot'], result_df['DMS_score'])[0], 4)
-        print(f"{dms_id}: R={r}")
+            ## Inference
+            all_poses, logits_table = misc.get_logits_table_sliding(q_seq, prot, msa, dms_df, model, tokenizer, str_tokenizer, start, mask_str=args.mask_str, disable_tqdm=False)
+            result_df = misc.get_scores_from_table(q_seq, logits_table, all_poses, dms_df, tokenizer, start, temp_mt=1.0, temp_wt=1.5)
+            
+            assert np.all(dms_df['mutant'] == result_df['Mutation'])
+            assert np.allclose(dms_df['DMS_score'] , result_df['GT_Score'], 1e-05, 1e-05)
+            dms_df['AIDO.Protein-RAG-16B-zeroshot'] = result_df['Pred_Score']
+            result_df = dms_df
+            
+            result_df.to_csv(join(args.output_path, f"{dms_id}.csv"), index=False)
+            r = round(spearmanr(result_df['AIDO.Protein-RAG-16B-zeroshot'], result_df['DMS_score'])[0], 4)
+            print(f"{dms_id}: R={r}")
+    else:
+        raise FileNotFoundError("No input for the DMS_id info. Specify --dms_ids or --reference_file")
+        
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--dms_ids", type=str, default=None, nargs="+")
+    parser.add_argument("--dms_ids", type=str, default=None, nargs="*")
+    parser.add_argument("--reference_file", type=str, default=None)
     parser.add_argument("--dms_index", type=int, default=None, help="Index of a single DMS_id in full list of assays (all_dms_ids)")
-    parser.add_argument("--input_data_path", type=str, default=f"{SCRIPT_PATH}", help="Input data path",)
+    # parser.add_argument("--input_data_path", type=str, default=f"{SCRIPT_PATH}", help="Input data path",)
+    parser.add_argument("--fasta_dir", required=True,
+                      help="Directory containing fasta files")
+    parser.add_argument("--msa_dir", required=True,
+                      help="Directory containing MSA files")
+    parser.add_argument("--dms_dir", required=True,
+                      help="Directory containing DMS CSV files")
+    parser.add_argument("--pdb_dir", required=False, default=None,
+                      help="Directory containing PDB files (only needed for esm3_open with use_structure=True)")
     parser.add_argument("--output_path", type=str, default=f"{SCRIPT_PATH}/output", help="Output path",)
     parser.add_argument("--hf_cache_location", type=str, default=None, help="Hugging Face cache directory for downloading models and tokenizers")
     parser.add_argument("--mask-str", action='store_true', help="Mask the structure input")
     args = parser.parse_args()
-    all_dms_ids = [n[:-7] for n in os.listdir(f'{args.input_data_path}/msa_data') if n.endswith('.txt.gz')]
+    all_dms_ids = [n for n in os.listdir(f'{args.dms_dir}') if n.endswith('.csv')]
+
+    print(args.reference_file)
     
     if args.dms_ids is None: 
         if args.dms_index is not None: # Compute scores for a single DMS id, indexed by dms_index
             args.dms_ids=[all_dms_ids[args.dms_index]]
         else:
-            args.dms_ids=all_dms_ids # Compute scores for all assays in ProteinGym
+            if args.reference_file is None:
+                args.dms_ids=all_dms_ids # Compute scores for all assays in ProteinGym
+                
     main(args)
