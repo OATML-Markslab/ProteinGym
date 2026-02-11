9bf6396281ebf226f318e700bfc3b31384d4fb06
Yilan Wang <yilanwang@Yilans-MBP.home.kola.sh>
2026-02-11 15:59:26 -0500

    enable scoring assays through a ref file and DMS indices

--- proteingym/baselines/prosst/compute_fitness.py
+++ proteingym/baselines/prosst/compute_fitness.py
@@ -72,7 +72,13 @@ def read_names(fasta_dir):
 
 def main():
     parser = ArgumentParser()
+    # protein entries
+    parser.add_argument("--reference_file", type=str, default=None, required=True, help="CSV file with DMS_id and target_seq columns")
+    parser.add_argument('--DMS_index', type=int, required=True, help='Index of a single DMS_id in full list of assays (all_dms_ids)')
+    # model
     parser.add_argument("--model_name", type=str, default="AI4Protein/ProSST-2048", nargs="+", required=True)
+    
+    # data directories
     parser.add_argument("--base_dir", type=str, default=None, help="Base directory containing all data",)
     parser.add_argument("--residue_dir", type=str, default=None, help="Directory containing FASTA files of residue sequences",)
     parser.add_argument("--structure_dir", type=str, default=None, help="Directory containing FASTA files of structure sequences",)
@@ -84,11 +90,15 @@ def main():
     os.makedirs(args.output_scores_folder, exist_ok=True)
     if args.base_dir:
         protein_names = read_names(f"{args.base_dir}/residue_sequence")
-    if args.residue_dir:
-        protein_names = read_names(args.residue_dir)
-    protein_names = sorted(protein_names)
-    print(protein_names)
-    print(f">>> total proteins: {len(protein_names)}")
+    #if args.residue_dir:
+    #    protein_names = read_names(args.residue_dir)
+    assert os.path.isfile(args.reference_file), 'MSA list file does not exist: {}'.format(args.reference_file)
+    reference_df = pd.read_csv(args.reference_file)
+    protein_name = reference_df['DMS_id'][args.DMS_index]
+    #protein_names = sorted(protein_names)
+    print(protein_name)
+    print("=====================================")
+    #print(f">>> total proteins: {len(protein_names)}")
     
     for model_name in args.model_name:
         print(f">>> Loading model {model_name}...")
@@ -98,37 +108,33 @@ def main():
         model = model.to(device)
         tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
         
-        for idx, protein_name in enumerate(protein_names):
-            print(f">>> Scoring {protein_name}, current {idx+1}/{len(protein_names)}...")
-            if args.base_dir:
-                residue_fasta = f"{args.base_dir}/residue_sequence/{protein_name}.fasta"
-                structure_fasta = f"{args.base_dir}/structure_sequence/{model_name.split('-')[-1]}/{protein_name}.fasta"
-                mutant_file = f"{args.base_dir}/substitutions/{protein_name}.csv"
-            if args.residue_dir:
-                residue_fasta = f"{args.residue_dir}/{protein_name}.fasta"
-            if args.structure_dir:
-                structure_fasta = f"{args.structure_dir}/{protein_name}.fasta"
-            if args.mutant_dir:
-                mutant_file = f"{args.mutant_dir}/{protein_name}.csv"
-            if os.path.exists(f"{args.output_scores_folder}/{protein_name}.csv"):
-                mutant_file = f"{args.output_scores_folder}/{protein_name}.csv"
-            mutant_df = pd.read_csv(mutant_file)
+        if args.base_dir:
+            residue_fasta = f"{args.base_dir}/residue_sequence/{protein_name}.fasta"
+            structure_fasta = f"{args.base_dir}/structure_sequence/{model_name.split('-')[-1]}/{protein_name}.fasta"
+            mutant_file = f"{args.base_dir}/substitutions/{protein_name}.csv"
+        # load data
+        residue_fasta = f"{args.residue_dir}/{protein_name}.fasta"
+        structure_fasta = f"{args.structure_dir}/{model_name.split('-')[-1]}/{protein_name}.fasta"
+        mutant_file = f"{args.mutant_dir}" + os.sep + reference_df['DMS_filename'][args.DMS_index]
+        if os.path.exists(f"{args.output_scores_folder}/{protein_name}.csv"):
+            mutant_file = f"{args.output_scores_folder}/{protein_name}.csv"
+        mutant_df = pd.read_csv(mutant_file)
+        
+        scores = score_protein(
+                model=model,
+                tokenizer=tokenizer,
+                residue_fasta=residue_fasta,
+                structure_fasta=structure_fasta,
+                mutant_df=mutant_df,
+            )
             
-            scores = score_protein(
-                    model=model,
-                    tokenizer=tokenizer,
-                    residue_fasta=residue_fasta,
-                    structure_fasta=structure_fasta,
-                    mutant_df=mutant_df,
-                )
-            
-            # remove the path from model_name
-            model_name = model_name.split("/")[-1]
-            mutant_df[model_name] = scores
-            corr = spearmanr(mutant_df["DMS_score"], mutant_df[model_name]).correlation
-            print(f">>> {model_name} on {protein_name}: {corr}")
+        # remove the path from model_name
+        model_name = model_name.split("/")[-1]
+        mutant_df[model_name] = scores
+       #  corr = spearmanr(mutant_df["DMS_score"], mutant_df[model_name]).correlation
+       #  print(f">>> {model_name} on {protein_name}: {corr}")
                 
-            mutant_df.to_csv(f"{args.output_scores_folder}/{protein_name}.csv", index=False)
+        mutant_df.to_csv(f"{args.output_scores_folder}/{protein_name}.csv", index=False)
 
 
 if __name__ == "__main__":
